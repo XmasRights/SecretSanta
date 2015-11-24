4396d787c4ac6096575a69a6029fadcc163db752
XmasRights <chris.fonseka@googlemail.com>
2015-11-24 00:55:58 +0000

    Cleared all the debugging messages

--- Santa.py
+++ Santa.py
@@ -17,44 +17,29 @@ def getList(filename):
 
 def generateSecretSanta(names):
     output = []
-    count  = 0
     while output == []:
         namesList = list(names)
         hatList   = list(names)
         recursiveHatPicking (namesList, hatList, output)
-        print "Output: " + str(output)
-        count = count + 1
-
-    print "Had to run recursion " + str(count) + " times"
-
     return output
 
 def recursiveHatPicking(names, hatOfNames, output):
-
-    print "Names: " + str(names)
-    print "Hat:   " + str(hatOfNames)
-
     if len(hatOfNames) == 0:
-        print "THE HAT IS EMPTY"
         return
 
     elif len(names) == 1 and hatOfNames[0] == names[0]:
         #if the last name in both lists are identical
-        print "NAMES ABORT ON " + str(names[0]) + " and " + hatOfNames[0]
         del output[:]
         return
 
     pick = removeRandomName(hatOfNames)
 
     if (pick != names[0]):
-        print names[0] + " has picked " + pick
         output.append (names[0] + " has picked " + pick)
         del names[0]
         recursiveHatPicking(names, hatOfNames, output)
     else:
-        print "Duplicate " + pick + " and " + names[0]
-        print "HAT OF NAMES"
-        print hatOfNames
+        #if person picked themselves, return pick to hat and repeat
         hatOfNames.append(pick)
         recursiveHatPicking(names, hatOfNames, output)
 
@@ -78,3 +63,5 @@ inFile     = sys.argv[1]
 namesList  = getList(inFile)
 
 generatedList = generateSecretSanta(namesList)
+
+print generatedList
